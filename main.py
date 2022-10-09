a5c7a12b0a0754f9167de69595461a65fc06d0f9
hiren-solanki1 <71432656+hiren-solanki1@users.noreply.github.com>
2022-10-09 17:17:55 +0530

    added new function

--- main.py
+++ main.py
@@ -20,3 +20,42 @@ num1 = 54
 num2 = 24
 
 print("The L.C.M. is", compute_lcm(num1, num2))
+
+
+def printSubSequences(STR, subSTR=""):
+	"""
+	function:
+		To print all subsequences of string
+		concept:
+			Pick and Don’t Pick
+		variables:
+			STR = string
+			subSTR = to store subsequence
+	"""
+	if len(STR) == 0:
+		print(subSTR, end=" ")
+		return
+
+	# we add adding 1st character in string
+	printSubSequences(STR[:-1], subSTR + STR[-1])
+	"""
+	Not adding first character of the string
+	because the concept of subsequence either
+	character will present or not
+	"""
+	printSubSequences(STR[:-1], subSTR)
+	return
+
+
+def main():
+	"""
+	main function to drive code
+	"""
+	STR = "abc"
+	printSubSequences(STR)
+
+
+if __name__ == "__main__":
+	main()
+
+# by itsvinayak
